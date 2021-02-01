47865d125635a43f1422d72158b6a4714a8d29f4
Vitalii Savchuk <savchuk.vitalia@gmail.com>
2021-02-02 00:29:44 +0200

    post creation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyDDzaEVjXaQ3wKZhS6Yc1_2kMtAgQP5wMQ'
+  apiKey: 'AIzaSyDDzaEVjXaQ3wKZhS6Yc1_2kMtAgQP5wMQ',
+  fbDbUrl: 'https://angular-blog-3ee7f-default-rtdb.firebaseio.com'
 };
