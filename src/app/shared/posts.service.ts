bb35907020ee8850a4dc04a9e74606066c02e863
Vitalii Savchuk <savchuk.vitalia@gmail.com>
2021-02-03 13:18:16 +0200

    added post deletion

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -33,4 +33,9 @@ export class PostsService {
           }));
       }));
   }
+
+  remove(id: string): Observable<void> {
+    return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`);
+
+  }
 }
