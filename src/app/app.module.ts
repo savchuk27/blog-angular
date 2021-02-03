a0dafbb47e5f9473e74b2fbf0ba10199bd08d798
Vitalii Savchuk <savchuk.vitalia@gmail.com>
2021-02-03 14:58:45 +0200

    added localization registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { PostComponent } from './shared/components/post/post.component';
 import {SharedModule} from './shared/shared.module';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AuthInterceptor} from './shared/auth.interceptor';
+import {registerLocaleData} from '@angular/common';
+import ruLocale from '@angular/common/locales/ru';
+
+
+registerLocaleData(ruLocale, 'ru');
 
 const INTERSEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
